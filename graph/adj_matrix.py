8aa0030d4e1fffacd4cafc0e3afd13299b65a334
Awanish-26 <awanishyadav996@gmail.com>
2024-01-02 17:20:53 +0530

    .

--- graph/adj_matrix.py
+++ graph/adj_matrix.py
@@ -1,5 +1,36 @@
 class Graph:
-    def __init__(self,vno):
-        self.vertex_count=vno
-        self.adj_matrix=[[0]*vno for e in range (vno)]
-    def add_edge(self,u,v,weight=1):
+    def __init__(self, vno):
+        self.vertex_count = vno
+        self.adj_matrix = [[0]*vno for e in range(vno)]
+
+    def add_edge(self, u, v, weight=1):
+        if 0 <= u < self.vertex_count and 0 <= v < self.vertex_count:
+            self.adj_matrix[u][v] = weight
+            self.adj_matrix[v][u] = weight
+        else:
+            print("Invalid vertex")
+
+    def remove_edge(self, u, v):
+        if 0 <= u < self.vertex_count and 0 <= v < self.vertex_count:
+            self.adj_matrix[u][v] = 0
+            self.adj_matrix[v][u] = 0
+        else:
+            print("Invalid vertex")
+
+    def has_edge(self, u, v):
+        if 0 <= u < self.vertex_count and 0 <= v < self.vertex_count:
+            return self.adj_matrix[u][v] != 0
+        else:
+            print("Invalid vertex")
+
+    def print_mat(self):
+        for row in self.adj_matrix:
+            print(" ".join(map(str, row)))
+
+
+t = Graph(5)
+t.add_edge(1, 2)
+t.add_edge(0, 1)
+t.add_edge(4, 2)
+t.add_edge(1, 2)
+t.print_mat()
